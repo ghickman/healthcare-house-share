fb90d5b57a3af658e7c821f37a9be131f1e6ee1c
George Hickman <george@ghickman.co.uk>
2016-01-30 14:55:05 +0000

    Set up static for dev

--- hhs/urls.py
+++ hhs/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 from .views import Home
@@ -22,4 +24,4 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     url(r'^$', Home.as_view(), name='home'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
