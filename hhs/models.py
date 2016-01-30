8de5f20bb2d37f32b4e717cc3ee874b5fe6b65a5
George Hickman <george@ghickman.co.uk>
2016-01-30 14:07:01 +0000

    Start adding models including a custom user model

---
+++ hhs/models.py
@@ -0,0 +1,54 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
+from django.db import models
+
+
+class House(models.Model):
+    address = models.TextField()
+    price = models.TextField()
+    room_count = models.IntegerField(default=1)
+    contract_end_date = models.DateField()
+    property_type = models.TextField()
+    parking_space_count = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.address
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """Creates and saves a User with the given email and password"""
+        user = self.model(
+            email=email,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        return self._create_user(email, password=password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields['is_superuser'] = True
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    name = models.TextField()
+    email = models.TextField(unique=True)
+    location = models.TextField()
+    destination = models.TextField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'email'
+
+    objects = UserManager()
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
