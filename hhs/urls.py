c745c3d9228734c06986964d15ab0bcbc8914a1a
George Hickman <george@ghickman.co.uk>
2016-01-30 12:04:34 +0000

    Add home page

--- hhs/urls.py
+++ hhs/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from .views import Home
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    url(r'^$', Home.as_view(), name='home'),
 ]
