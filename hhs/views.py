329a67d555e1d56d4ead1100e501e4d79a5d1521
George Hickman <george@ghickman.co.uk>
2016-01-31 09:50:40 +0000

    Consume add property form

--- hhs/views.py
+++ hhs/views.py
@@ -1,20 +1,40 @@
+from django.db import transaction
 from django.views.generic import FormView, TemplateView
-from django.core.urlresolvers import reverse_lazy
 
 from .forms import AddHouseForm
+from .models import Contract, House
 
 
 class AddHouse(FormView):
     template_name = 'add_house.html'
     form_class = AddHouseForm
-    success_url = reverse_lazy('/')
+    success_url = '/'
+
+    def form_valid(self, form):
+        with transaction.atomic():
+            house = House.objects.create(
+                address=form.cleaned_data['address'],
+                property_type=form.cleaned_data['property_type'],
+                room_count=form.cleaned_data['room_count'],
+                parking_space_count=form.cleaned_data['parking_space_count'],
+            )
+
+            Contract.objects.create(
+                house=house,
+                user=self.request.user,
+                price=form.cleaned_data['price'],
+                end_date=form.cleaned_data['available_date'],
+            )
+        return super().form_valid(form)
 
 
 class Home(TemplateView):
     template_name = 'index.html'
 
+
 class Search(TemplateView):
     template_name = 'search.html'
+
 
 class Property(TemplateView):
     template_name = 'property.html'
