f8bf0d4939fc407069be007a87de3d359f71789f
George Hickman <george@ghickman.co.uk>
2016-01-31 09:49:55 +0000

    Consume js datepicker dateformat

--- hhs/forms.py
+++ hhs/forms.py
@@ -11,6 +11,6 @@ class AddHouseForm(forms.Form):
     address = forms.CharField()
     property_type = forms.ChoiceField(choices=house_type_choices)
     room_count = forms.IntegerField()
-    available_date = forms.DateField()
+    available_date = forms.DateField(input_formats=['%a %b %d %Y'])
     price = forms.IntegerField()
     notes = forms.CharField(widget=forms.Textarea())
